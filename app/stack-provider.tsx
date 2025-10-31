0fb498fe833a1806f677a3a94dd2f41158afe0fa
Jean C Orelien <orelien.j.coti@gmail.com>
2025-10-30 20:32:49 -0400

    Add tokenStore to StackClientApp config

--- app/stack-provider.tsx
+++ app/stack-provider.tsx
@@ -3,6 +3,7 @@
 import { StackProvider, StackClientApp } from "@stackframe/stack";
 
 const stackApp = new StackClientApp({
+  tokenStore: "nextjs-cookie",
   projectId: process.env.NEXT_PUBLIC_STACK_PROJECT_ID!,
   publishableClientKey: process.env.NEXT_PUBLIC_STACK_PUBLISHABLE_CLIENT_KEY!,
 });
